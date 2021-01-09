86d963153b30edc530db577f0bf1f61ff41eb99e
z00md <z00md@z00md.com>
2021-01-09 14:26:21 +0530

    Added map and logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
 import './App.css';
+import './Map.css';
+// import Map from './india.svg';
+import IndiaMap from './IndiaMap';
+import { useState } from "react";
+import data from "./data.js";
+
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [question, setQuestion] = useState(0);
+  const [gameEnded, setGameEnded] = useState(0);
+
+  function nextQuestion() {
+    setQuestion(question + 1);
+  }
+  function updateScore(answer) {
+    if (answer === data[question].stateName) {
+      setScore(score + 1);
+    }
+  }
+  function endGame() {
+    setGameEnded(1);
+  }
+  function onMapClick(e) {
+    let title = e.target.getAttribute("title");
+    if (title) {
+      let clicked = e.target.getAttribute("title");
+      if (!gameEnded) {
+        updateScore(clicked);
+      }
+      if (question >= data.length - 1) {
+        endGame();
+      } else {
+        nextQuestion();
+      }
+    }
+  }
+  function startGame(){
+    setQuestion(0);
+    setScore(0);
+    setGameEnded(0);
+  }
+
+  let questionText = gameEnded ? "Completed!" : data[question].cmName;
+
   return (
     <div className="App">
-      <header className="App-header">
-        React App Deployment Test
-      </header>
+      <div className="header">
+        <div className="title">Do you know Chief Ministers of India?</div>
+        <br/>
+        <div className="score">You know - {score}/{data.length}</div>
+        <br/>
+        <div className="question">{questionText}</div>
+        <br/>
+        {gameEnded === 1 && <div onClick={startGame}>Restart</div>}
+      </div>
+      <div className="map-container">
+        <IndiaMap onClick={onMapClick}/>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,123 @@
+var data = [
+  {
+    stateName: "Andhra Pradesh",
+    cmName: "Y. S. Jaganmohan Reddy"
+  },
+  {
+    stateName: "Arunachal Pradesh",
+    cmName: "Pema Khandu"
+  },
+  {
+    stateName: "Assam",
+    cmName: "Sarbananda Sonowal"
+  },
+  {
+    stateName: "Bihar",
+    cmName: "Nitish Kumar"
+  },
+  {
+    stateName: "Chhattisgarh",
+    cmName: "Bhupesh Baghel"
+  },
+  {
+    stateName: "Delhi",
+    cmName: "Arvind Kejriwal"
+  },  {
+    stateName: "Goa",
+    cmName: "Pramod Sawant"
+  },
+  {
+    stateName: "Gujarat",
+    cmName: "Vijay Rupani"
+  },
+  {
+    stateName: "Haryana",
+    cmName: "Manohar Lal Khattar"
+  },
+  {
+    stateName: "Himachal Pradesh",
+    cmName: "Jai Ram Thakur"
+  },
+  {
+    stateName: "Jammu and Kashmir",
+    cmName: "(President's rule)"
+  },
+  {
+    stateName: "Jharkhand",
+    cmName: "Hemant Soren"
+  },
+  {
+    stateName: "Karnataka",
+    cmName: "B. S. Yediyurappa"
+  },
+  {
+    stateName: "Kerala",
+    cmName: "Pinarayi Vijayan"
+  },
+  {
+    stateName: "Madhya Pradesh",
+    cmName: "Shivraj Singh Chouhan"
+  },
+  {
+    stateName: "Maharashtra",
+    cmName: "Uddhav Thackeray"
+  },
+  {
+    stateName: "Manipur",
+    cmName: "N. Biren Singh"
+  },
+  {
+    stateName: "Meghalaya",
+    cmName: "Conrad Sangma"
+  },
+  {
+    stateName: "Mizoram",
+    cmName: "Zoramthanga"
+  },
+  {
+    stateName: "Nagaland",
+    cmName: "Neiphiu Rio"
+  },
+  {
+    stateName: "Odisha",
+    cmName: "Naveen Patnaik"
+  },
+  {
+    stateName: "Punjab",
+    cmName: "Amarinder Singh"
+  },
+  {
+    stateName: "Rajasthan",
+    cmName: "Ashok Gehlot"
+  },
+  {
+    stateName: "Sikkim",
+    cmName: "Prem Singh Tamang"
+  },
+  {
+    stateName: "Tamil Nadu",
+    cmName: "Edappadi K. Palaniswami"
+  },
+  {
+    stateName: "Telangana",
+    cmName: "K. Chandrashekhar Rao"
+  },
+  {
+    stateName: "Tripura",
+    cmName: "Biplab Kumar Deb"
+  },
+  {
+    stateName: "Uttar Pradesh",
+    cmName: "Yogi Adityanath"
+  },
+  {
+    stateName: "Uttarakhand",
+    cmName: "Trivendra Singh Rawat"
+  },
+  {
+    stateName: "West Bengal",
+    cmName: "Mamata Banerjee"
+  }
+];
+
+export default data;
